e66809d963b2ffe9867ba15e5aab3455f8dbdae3
minyoung0 <hmylove01@naver.com>
2025-09-09 22:02:31 +0900

    Initialize local and merge with origin/main

---
+++ coupleDiary/src/main/java/com/example/coupleDiary/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.example.coupleDiary.controller;
+
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String mainPage(){
+        return "index";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage(Model model){
+        return "/user/login";
+    }
+}
